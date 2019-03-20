83fbcc14d5fb2680ba8b93d23e98ff74fb12187b
LAB <838537276@qq.com>
2019-03-20 19:59:09 +0800

    添加静态文件,以及底层dao

--- src/main/java/com/gdou/springboot/config/MymvcConfig.java
+++ src/main/java/com/gdou/springboot/config/MymvcConfig.java
@@ -1,6 +1,7 @@
 package com.gdou.springboot.config;
 
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -19,5 +20,17 @@ public class MymvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
             registry.addViewController("/gdou").setViewName("success");
+    }
+
+    @Bean
+    public WebMvcConfigurer webMvcConfigurer(){
+        WebMvcConfigurer webMvcConfigurer = new WebMvcConfigurer() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/").setViewName("index");
+                registry.addViewController("/index.html").setViewName("index");
+            }
+        };
+        return webMvcConfigurer;
     }
 }
