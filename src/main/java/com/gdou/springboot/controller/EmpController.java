6e22f56519b37e516c5fbb32698e33b2230d5cff
LAB <838537276@qq.com>
2019-03-23 16:24:29 +0800

    2019-3-23 实现了删除功能

--- src/main/java/com/gdou/springboot/controller/EmpController.java
+++ src/main/java/com/gdou/springboot/controller/EmpController.java
@@ -7,10 +7,7 @@ import com.gdou.springboot.entities.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Collection;
 
@@ -56,5 +53,10 @@ public class EmpController {
     public String updateEmp(Employee employee){
         employeeDao.save(employee);
          return "redirect:/emps";
+    }
+    @DeleteMapping("/emp/{id}")
+    public String deleteEmp(@PathVariable("id") Integer id){
+        employeeDao.delete(id);
+        return "redirect:/emps";
     }
 }
